924de18322816e3449c5322d936e374a0d895b10
Achmad <achfaisal96@gmail.com>
2023-07-20 19:25:49 +0700

    change .env and api.js

--- src/api.js
+++ src/api.js
@@ -4,14 +4,14 @@ const apiKey = process.env.REACT_APP_APIKEY;
 
 export const getMovieList = async () => {
   const movie = await axios.get(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+    `${process.env.REACT_APP_BASEURL}/movie/popular?api_key=${apiKey}`
   );
   return movie.data.results;
 };
 
 export const searchMovie = async (q) => {
   const search = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?query=${q}&api_key=${apiKey}`
+    `${process.env.REACT_APP_BASEURL}/search/movie?query=${q}&api_key=${apiKey}`
   );
   console.log(q);
   return search.data;
